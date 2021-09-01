021a423cc0e9b86e584c8999f46b0fd98e095a53
WonYoung Choi <wy80.choi@samsung.com>
2021-09-01 12:30:11 +0900

    Remove hard-coded branch name in build-docker.sh

--- ci/docker/tizen/build-docker.sh
+++ ci/docker/tizen/build-docker.sh
@@ -2,7 +2,6 @@
 
 IMAGE_NAME=ghcr.io/flutter-tizen/build-engine
 IMAGE_TAG=latest
-GIT_BRANCH=flutter-2.2.1-tizen
 
 docker pull $IMAGE_NAME:$IMAGE_TAG
-docker build --build-arg GIT_BRANCH=$GIT_BRANCH --tag $IMAGE_NAME:$IMAGE_TAG .
+docker build --tag $IMAGE_NAME:$IMAGE_TAG .
